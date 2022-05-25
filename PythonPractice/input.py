c6619249075549be651edb903643c980d5c0648a
RakeshKombali <Rakeshkombali@gmail.com>
2022-05-25 09:01:57 +0530

    Added golang

---
+++ PythonPractice/input.py
@@ -0,0 +1,33 @@
+import sys, atexit,io
+
+# g= input("enther the number: ")
+# print(g,type(g))
+# f= int(input("enter the string: "))
+# print(f,type(f))
+# x,y=input(),input()
+# print (x,y)
+
+
+#list comprehension
+# a,b=[input(),input()]
+# print(a,b)
+# x,y=[int(x) for x in input().split()]
+# print(x,y, type(x),type(y))
+
+# g,f=map(int,input().split())
+# print(g,f)
+
+# x=sys.stdin.readline().strip
+# print(x)
+
+# def get_ints():
+#     return list(map(int, sys.stdin.readline().strip().split()))
+
+# arr=get_ints()
+#a,b,c,d = get_ints()
+# print('a:',a)
+# print('b:',b)
+# print('c:',c)
+# print('d:',d)
+
+# print(arr)
