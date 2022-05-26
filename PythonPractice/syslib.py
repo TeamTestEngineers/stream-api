7df96fdef5948bf1dde17cfbe89ae35863eecd11
RakeshKombali <Rakeshkombali@gmail.com>
2022-05-26 10:01:48 +0530

    rebasing the master.

---
+++ PythonPractice/syslib.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.version)
+
+# for lines in sys.stdin:
+#     if 'q' == lines.rstrip():
+#         break
+#     print(f'Input: {lines}')
+
+# print("Exit")
+
+sys.stdout.write('Geeks')
+print(sys.path)
